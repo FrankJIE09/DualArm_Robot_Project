a0ffb34439ea752d276c00287c0d8949ee0d1748
frank <12132265@mail.sustech.edu.cn>
2025-02-12 10:38:44 +0800

    update

--- software/motion_control/grasp_hans_robot.py
+++ software/motion_control/grasp_hans_robot.py
@@ -1,5 +1,8 @@
 import yaml
 import time
+
+from pyorbbecsdk import Context
+
 from hans_robot.CPS import CPSClient  # 假设有一个机器人控制的库
 from software.vision.orbbec_camera import OrbbecCamera  # Realsense 相机类
 from jodell_gripper.RG.rg_api import ClawOperation  # 手爪控制类
@@ -28,29 +31,36 @@ class RobotController:
         self.client.HRIF_Connect2Controller(self.box_id)
 
         # 获取当前位置
-        self.current_pose = self.client.read_pos()  # 读取当前位置
+        self.current_pose = self.client.getTCPPose()  # 读取当前位置
         print(f"Initial Current Pose: {self.current_pose}")
 
         # 读取目标位置
         self.positions = self.read_position()
         self.joint = self.read_position("joint")
 
+        """初始化所有已连接的相机"""
+        context = Context()  # 创建一个新的上下文对象
+        device_list = context.query_devices()
+        print(device_list)
+        device_id_list = list(range(len(device_list)))
         # 初始化 Realsense 相机
-        self.camera = OrbbecCamera(device_id=0)
+        devices = [OrbbecCamera(device_id) for device_id in device_id_list]  # 为每个设备ID创建一个Camera对象
+
+        self.camera = devices[0]
         # 读取标定信息
         self.calibration_data = self.read_calibration()
         # 初始化手爪控制
         self.gripper = ClawOperation(com_port="/dev/ttyUSB0", baud_rate=115200)  # 假设串口设备在 /dev/ttyUSB0
-
-        # self.gripper.initialize_gripper()  # 初始化手爪
+        self.gripper.connect()
 
         # 初始化 YOLO 模型
         self.model = YOLO(model_path, task="val")  # 确保使用正确的模型路径和设备
 
         # 设置置信度阈值
         self.conf_threshold = conf_threshold
-        self.gripper.enable_claw(claw_id=9, enable=True)
-        self.gripper.run_with_param(claw_id=9, force=255, speed=255, position=255)
+        # self.gripper.enable_claw(claw_id=9, enable=True)
+
+        self.gripper.open()
         self._stop_event = threading.Event()
 
     def read_calibration(self, tag="hand_eye_transformation_matrix"):
@@ -69,11 +79,12 @@ class RobotController:
         # 计算检测到的目标物体的3D位置的函数。
 
         # 假设检测到的第一个目标为所需目标，获取其四个顶点的坐标
-        x1, y1, x2, y2, x3, y3, x4, y4 = detection.flatten()
+        # 展开detection并获取四个角的坐标
+        x1, y1, x2, y2 = detection.flatten()
 
         # 计算目标的中心点
-        center_x = int((x1 + x2 + x3 + x4) // 4)
-        center_y = int((y1 + y2 + y3 + y4) // 4)
+        center_x = int((x1 + x2) // 2)
+        center_y = int((y1 + y2) // 2)
         # 获取中心点的深度值
         center_depth = self.camera.get_depth_for_color_pixel(depth_frame=depth_frame,
                                                              color_point=[center_x, center_y])
@@ -82,19 +93,13 @@ class RobotController:
         # 计算实际深度（Z轴）
         real_z = center_depth / 1000
 
-        fx, fy = self.camera.fx, self.camera.fy  # 获取相机的焦距参数
+        fx, fy = self.camera.rgb_fx, self.camera.rgb_fy  # 获取相机的焦距参数
         real_x = (center_x - self.camera.ppx) * real_z / fx  # 计算目标的实际X坐标
         real_y = (center_y - self.camera.ppy) * real_z / fy  # 计算目标的实际Y坐标
 
-        # 计算目标的旋转角度 theta
-        delta_x = x2 - x1
-        delta_y = y2 - y1
-        theta = np.arctan2(delta_y, delta_x)  # 计算目标的旋转角度（弧度）
-        theta_degrees = np.degrees(theta)  # 将角度从弧度转换为度数
-
         print(
-            f"目标位置（使用depth_image计算）（相机坐标系）：x={real_x}, y={real_y}, z={real_z}, 旋转角度 theta={theta_degrees}°")
-        return real_x, real_y, real_z, theta_degrees  # 返回目标的实际坐标和旋转角度
+            f"目标位置（使用depth_image计算）（相机坐标系）：x={real_x}, y={real_y}, z={real_z}°")
+        return real_x, real_y, real_z, 0  # 返回目标的实际坐标和旋转角度
 
     def detect_object(self, color_image, show=False):
         """
@@ -116,11 +121,28 @@ class RobotController:
             for item in results:
                 item.show(font_size=1, line_width=2)
 
-        # 假设 results[0].obb.xyxyxyxy 是边界框坐标
-        obb_coordinates = results[0].obb.xyxyxyxy.cpu().numpy()
+        # 获取结果中的边界框、标签和置信度
+        boxes = results[0].boxes.xyxy.cpu().numpy()  # 获取所有边界框
+        confidences = results[0].boxes.conf.cpu().numpy()  # 获取每个框的置信度
+        labels = results[0].names  # 获取物体标签
 
-        # 返回物体的中心坐标
-        return obb_coordinates
+        # 记录最大置信度的 'bottle_head' 边界框
+        max_confidence = -1
+        max_confidence_box = None
+
+        # 遍历所有边界框，找到 'bottle_head' 并筛选出置信度最高的
+        for idx, label in enumerate(results[0].boxes.cls.cpu().numpy()):
+            if labels.get(int(label)) == 'bottle_head' and confidences[idx] >= 0.9:
+                if confidences[idx] > max_confidence:
+                    max_confidence = confidences[idx]
+                    max_confidence_box = boxes[idx]
+
+        # 输出置信度最高的 'bottle_head' 边界框（xyxy）
+        if max_confidence_box is not None:
+            return max_confidence_box
+        else:
+            print("没有找到置信度高于0.9的 'bottle_head'。")
+            return None
 
     def perform_grab(self, object_center):
         """执行抓取动作"""
@@ -186,7 +208,7 @@ class RobotController:
             # 返回平移和旋转欧拉角
             return np.concatenate([translation, euler_angles])
 
-        def create_end_to_finger_matrix(x=0.0, y=0.03, z=0.27, theta=0):
+        def create_end_to_finger_matrix(x=0.0, y=0.0, z=0.18, theta=0):
             """
             创建一个 4x4 齐次变换矩阵。
 
@@ -210,18 +232,21 @@ class RobotController:
         theta = float(object_center[-1])
         object_center = np.array(object_center[0:3])
         T_camera_to_target = create_transformation_matrix(object_center, theta)
-        T_base_to_end = euler_to_transformation_matrix(self.client.read_pos())
+        T_base_to_end = euler_to_transformation_matrix(self.client.getTCPPose())
         T_end_to_camera = np.array(self.read_calibration())
         base_to_target = T_base_to_end @ T_end_to_camera @ T_camera_to_target
         T_end_to_finger = create_end_to_finger_matrix()
         aim_base_to_end = base_to_target @ np.linalg.inv(T_end_to_finger)
         aim_pose = transformation_matrix_to_xyzrpy(aim_base_to_end)
+        aim_pose[3:6] = 119, 17, -166
         pre_grasp_pose = aim_pose.copy()  # 抓取前上方100mm
         grasp_pose = aim_pose.copy()  # 抓取时的高度
         after_grasp_pose = aim_pose.copy()  # 抓取时的高度
-        pre_grasp_pose[2] = 620
-        grasp_pose[2] = 480  # 抓取时的高度
-        after_grasp_pose[2] = 750
+        pre_grasp_pose[1] = pre_grasp_pose[1]-100
+        grasp_pose[1] = grasp_pose[1]+20  # 抓取时的高度
+        grasp_pose[2] = grasp_pose[2]-20  # 抓取时的高度
+
+        after_grasp_pose[2] = after_grasp_pose[2]+100
 
         # 移动到物体上方100mm
         print("Moving above the object...")
@@ -230,11 +255,10 @@ class RobotController:
         # 向下移动到抓取高度
         print("Moving down to grab the object...")
         self.client.move_robot(grasp_pose)
-
+        self.gripper.close()
         # 控制手爪抓取物体
         print("Closing gripper to grab the object...")
         # self.gripper.set_force(50)  # 设置夹持力
-        self.gripper.run_with_param(claw_id=9, force=255, speed=255, position=0)
         time.sleep(1)
 
         # 抓取完成后升高100mm
@@ -247,15 +271,15 @@ class RobotController:
     def release_object(self):
         """释放物体"""
         print("Opening gripper to release the object...")
-        self.gripper.run_with_param(claw_id=9, force=255, speed=255, position=255)
+        self.gripper.open()
 
     def pre_execute(self):
-        if self.current_pose[2] < 450:
-            print(f"Current height {self.current_pose[2]} is less than 500. Moving to 500...")
-            self.client.move_robot(target_pose=[self.current_pose[0], self.current_pose[1], 450, 0, 0, 0])
+        # if self.current_pose[2] < 450:
+        #     print(f"Current height {self.current_pose[2]} is less than 500. Moving to 500...")
+        #     self.client.move_robot(target_pose=[self.current_pose[0], self.current_pose[1], 450, 0, 0, 0])
         init_joint = self.joint['init_joint']
-        self.client.moveJ_robot(target_joint=init_joint)
-        self.camera.adjust_exposure_based_on_brightness(target_brightness=158)
+        self.client.moveByJoint(target_joint=init_joint)
+        # self.camera.adjust_exposure_based_on_brightness(target_brightness=158)
 
     def execute(self):
         """执行完整的任务流程"""
@@ -266,40 +290,19 @@ class RobotController:
             time.sleep(0.1)
             color_image, depth_image, depth_frame = self.camera.get_frames()
             print("Captured frames from RealSense.")
-            detection = self.detect_object(color_image)
-            # 在掩码图像上绘制矩形区域以保留
-            if detection is None or detection is [] or detection.shape[0] == 0:
-                print("Object detection failed. Exiting.")
-                continue
-            for item in detection:
-                belt_area = item
-                # 创建掩码图像
-                mask = np.zeros_like(color_image)
-                x_min = max(0, int(belt_area[:, 0].min() - 50))
-                y_min = max(0, int(belt_area[:, 1].min() - 50))
-                x_max = min(color_image.shape[1], int(belt_area[:, 0].max() + 50))
-                y_max = min(color_image.shape[0], int(belt_area[:, 1].max() + 50))
-
-                # 保留目标区域，其余部分设置为黑色
-                mask[y_min:y_max, x_min:x_max, :] = color_image[y_min:y_max, x_min:x_max, :]
-                detection = self.detect_belt(mask)
-                # 在掩码图像上绘制矩形区域以保留
-                if detection is None or detection is [] or detection.shape[0] == 0:
-                    print("Object detection failed. Exiting.")
-                    continue
-                break
+            detection = self.detect_object(color_image, show=True)
             if detection is None or detection is [] or detection.shape[0] == 0:
                 print("Object detection failed. Exiting.")
                 return
             break
-        object_center = self.calculate_position(detection[0], depth_frame)
+        object_center = self.calculate_position(detection, depth_frame)
 
         # 3. 执行抓取动作
         self.perform_grab(object_center)
 
         # 4. 移动到目标位置并释放物体
-        gift_joint_release = self.joint['gift_joint_release']
-        self.client.moveJ_robot(gift_joint_release)
+        gift_joint_release = self.joint['release_joint']
+        self.client.moveByJoint(gift_joint_release)
 
     def draw_circle(self, boxID, rbtID, center, radius, y_height, num_points=36):
         """
@@ -338,7 +341,7 @@ class RobotController:
 
     def pre_circle(self):
         cycle_joint_init = self.joint["cycle_joint_init"]
-        self.client.moveJ_robot(cycle_joint_init)
+        self.client.moveByJoint(cycle_joint_init)
 
     def circle(self):
         # Setup parameters for the circle drawing
@@ -354,7 +357,7 @@ class RobotController:
 
     def pre_greet(self):
         greet_joint_init = self.joint["greet_joint_init"]
-        self.client.moveJ_robot(greet_joint_init)
+        self.client.moveByJoint(greet_joint_init)
 
     def greet(self):
         # greet_point = self.positions["greet"]
@@ -362,15 +365,15 @@ class RobotController:
         greet_joint_init = self.joint["greet_joint_init"]
         greet_joint_left = self.joint["greet_joint_left"]
         greet_joint_right = self.joint["greet_joint_right"]
-        self.client.moveJ_robot(greet_joint_left)
-        self.client.moveJ_robot(greet_joint_right)
-        self.client.moveJ_robot(greet_joint_init)
+        self.client.moveByJoint(greet_joint_left)
+        self.client.moveByJoint(greet_joint_right)
+        self.client.moveByJoint(greet_joint_init)
 
         # self.client.move_robot(greet_point)
 
     def pre_story(self):
         greet_point = self.joint["greet_joint_init"]
-        self.client.moveJ_robot(greet_point)
+        self.client.moveByJoint(greet_point)
 
     def story(self):
         while not self._stop_event.is_set():
--- software/vision/orbbec_camera.py
+++ software/vision/orbbec_camera.py
@@ -5,9 +5,10 @@ import yaml
 import json
 import cv2
 from software.vision.utils import frame_to_bgr_image, frame_to_rgb_frame
+
 ESC_KEY = 27
-MIN_DEPTH = 20  # 20mm
-MAX_DEPTH = 10000  # 10000mm
+MIN_DEPTH = 100  # 20mm
+MAX_DEPTH = 500  # 10000mm
 
 
 class TemporalFilter:
@@ -46,6 +47,11 @@ class OrbbecCamera:
 
         self.param = self.pipeline.get_camera_param()
         print(self.param)
+        self.device.set_depth_work_mode("High Accuracy")
+        self.set_auto_exposure(True)
+        self.device.set_bool_property(OBPropertyID.OB_PROP_DISPARITY_TO_DEPTH_BOOL,True)
+        self.start_stream()
+
         self.depth_intrinsic = self.param.depth_intrinsic
         self.depth_fx = self.depth_intrinsic.fx  # 焦距 X
         self.depth_fy = self.depth_intrinsic.fy  # 焦距 Y
@@ -54,11 +60,14 @@ class OrbbecCamera:
         self.depth_distortion = self.param.depth_distortion  # 深度相机畸变系数
 
         self.rgb_intrinsic = self.param.rgb_intrinsic
-        self.rgb_fx = self.rgb_intrinsic.fx  # RGB 相机焦距 X
-        self.rgb_fy = self.rgb_intrinsic.fy  # RGB 相机焦距 Y
-        self.rgb_ppx = self.rgb_intrinsic.cx  # RGB 相机主点 X
-        self.rgb_ppy = self.rgb_intrinsic.cy  # RGB 相机主点 Y
+        self.fx = self.rgb_intrinsic.fx  # RGB 相机焦距 X
+        self.fy = self.rgb_intrinsic.fy  # RGB 相机焦距 Y
+        self.ppx = self.rgb_intrinsic.cx  # RGB 相机主点 X
+        self.ppy = self.rgb_intrinsic.cy  # RGB 相机主点 Y
         self.rgb_distortion = self.param.rgb_distortion  # RGB 相机畸变系数
+
+
+
 
     def get_device_name(self):
         return self.device_info.get_name()
@@ -92,25 +101,6 @@ class OrbbecCamera:
         new_exposure = curr_exposure + adjustment
         # 设置新的曝光值
         self.set_exposure(new_exposure)
-
-    # 获取当前的彩色相机增益值
-    def get_current_gain(self):
-        return self.device.get_int_property(OBPropertyID.OB_PROP_COLOR_GAIN_INT)
-
-    # 设置新的彩色相机增益值
-    def set_gain(self, gain_value: int):
-        self.device.set_int_property(OBPropertyID.OB_PROP_COLOR_GAIN_INT, gain_value)
-        print(f"Gain set to: {gain_value}")
-
-    # 开关激光
-    def set_laser(self, laser: bool):
-        self.device.set_bool_property(OBPropertyID.OB_PROP_LASER_BOOL, laser)
-        print(f"Laser set to: {laser}")
-
-    # 开关LDP
-    def set_ldp(self, ldp: bool):
-        self.device.set_bool_property(OBPropertyID.OB_PROP_LDP_BOOL, ldp)
-        print(f"LDP set to: {ldp}")
 
     # 开关软件滤波
     def set_software_filter(self, soft_filter: bool):
@@ -171,7 +161,14 @@ class OrbbecCamera:
             if point_clouds is None:
                 print("No point clouds")
             break
-        return depth_frame, color_frame, point_clouds
+        color_image = self.color_frame2color_image(color_frame)
+        depth_image = self.depth_frame2depth_data(depth_frame, filter_on=True)
+
+        return color_image, depth_image, depth_frame
+
+    def get_depth_for_color_pixel(self, depth_frame, color_point):
+        depth_data = self.depth_frame2depth_data(depth_frame, filter_on=True)
+        return depth_data[color_point[1], color_point[0]]
 
     def color_frame2color_image(self, color_frame):
         color_image = frame_to_bgr_image(color_frame)
@@ -207,10 +204,9 @@ class OrbbecCamera:
         max_exposure = 10000
 
         while True:
-            _, color_frame, _ = self.get_frames()  # 获取一帧图像
+            color_image, depth_image, depth_frame = self.get_frames()  # 获取一帧图像
 
             # 根据人眼感知计算亮度（加权平均法）
-            color_image = self.color_frame2color_image(color_frame)
             print(color_image.shape)
             current_brightness = (
                     0.299 * color_image[:, :, 2] +  # Red 通道
@@ -244,68 +240,6 @@ class OrbbecCamera:
         except Exception as e:
             raise RuntimeError(f"加载外参失败: {e}")
 
-    def calculate_position_from_depth(self, u, v, Z):
-        """
-        计算给定像素点 (u, v) 在相机坐标系下的 3D 位置。
-
-        参数：
-        self        - 相机对象，包含内参
-        u, v        - 像素点坐标（彩色/深度图像）
-        depth_data  - 深度图数据（单位：米），形状为 (H, W)
-
-        返回：
-        (X, Y, Z) - 该像素点在相机坐标系下的 3D 坐标
-        """
-        # 获取深度值（以米为单位）
-
-        # 如果深度值无效（通常 0 或 NaN），返回 None
-        if Z <= 0 or np.isnan(Z):
-            Z = 0
-
-        # 相机内参
-        fx = self.depth_fx
-        fy = self.depth_fy
-        cx = self.depth_ppx
-        cy = self.depth_ppy
-
-        # 计算 3D 坐标
-        X = (u - cx) * Z / fx
-        Y = (v - cy) * Z / fy
-
-        return (X, Y, Z)
-
-    def calculate_position_from_point_clouds(self, u, v, point_clouds):
-        """
-        计算给定像素点 (u, v) 在相机坐标系下的 3D 位置。
-
-        参数：
-        self        - 相机对象，包含内参
-        u, v        - 像素点坐标（彩色/深度图像）
-        depth_data  - 深度图数据（单位：米），形状为 (H, W)
-
-        返回：
-        (X, Y, Z) - 该像素点在相机坐标系下的 3D 坐标
-        """
-        # 获取深度值（以米为单位）
-        Z = point_clouds[v, u][2]
-
-        # 如果深度值无效（通常 0 或 NaN），返回 None
-        if Z <= 0 or np.isnan(Z):
-            return 0, 0, 0
-
-        # 相机内参
-        fx = self.depth_fx
-        fy = self.depth_fy
-        cx = self.depth_ppx
-        cy = self.depth_ppy
-
-        # 计算 3D 坐标
-        X = (u - cx) * Z / fx
-        Y = (v - cy) * Z / fy
-
-        return X, Y, Z
-
-
 def initialize_connected_cameras():
     """初始化所有已连接的相机"""
     context = Context()  # 创建一个新的上下文对象
@@ -332,12 +266,12 @@ def main():
 
     # 假设只使用第一个相机
     camera = cameras[0]
-    camera.start_stream()
+    # camera.start_stream()
     try:
         # 获取一帧图像数据
         while True:
-            depth_frame, color_frame, _ = camera.get_frames()
-            color_image = camera.color_frame2color_image(color_frame)
+            color_image, depth_image, depth_frame = camera.get_frames()
+
             if color_image is None:
                 print("failed to convert frame to image")
                 continue
@@ -345,14 +279,6 @@ def main():
 
             camera.show_depth_frame(depth_frame)
             cv2.waitKey(1)
-            depth_data = camera.depth_frame2depth_data(depth_frame)
-            # 示例
-            center_y = int(color_frame.get_height() / 2)
-            center_x = int(color_frame.get_width() / 2)
-            center_distance = depth_data[center_y, center_x]
-            # 计算相机坐标系中的 3D 坐标
-            # camera_coordinates = camera.pixel_to_camera_coordinates(center_x, center_y, center_distance)
-            # print(f"相对相机坐标: {camera_coordinates}")
 
 
     finally:
